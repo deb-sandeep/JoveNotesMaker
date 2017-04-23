d85af8ba02c714a865b4a78ea281f0cbdfd9f03f
Sandeep Deb <deb.sandeep@gmail.com>
2017-04-23 01:18:32 +0000

    Started working on indexing jn source content

--- src/main/java/com/sandy/jnmaker/JoveNotesMaker.java
+++ src/main/java/com/sandy/jnmaker/JoveNotesMaker.java
@@ -1,12 +1,27 @@
 package com.sandy.jnmaker ;
 
-import static com.sandy.jnmaker.util.ObjectRepository.* ;
+import static com.sandy.jnmaker.util.ObjectRepository.getProjectManager ;
+import static com.sandy.jnmaker.util.ObjectRepository.getWkspManager ;
+import static com.sandy.jnmaker.util.ObjectRepository.setApp ;
+import static com.sandy.jnmaker.util.ObjectRepository.setAppConfig ;
+import static com.sandy.jnmaker.util.ObjectRepository.setBus ;
+import static com.sandy.jnmaker.util.ObjectRepository.setMainFrame ;
+import static com.sandy.jnmaker.util.ObjectRepository.setObjectFactory ;
+import static com.sandy.jnmaker.util.ObjectRepository.setProjectManager ;
+import static com.sandy.jnmaker.util.ObjectRepository.setStateMgr ;
+import static com.sandy.jnmaker.util.ObjectRepository.setUiActions ;
+import static com.sandy.jnmaker.util.ObjectRepository.setWkspManager ;
+import static com.sandy.jnmaker.util.ObjectRepository.setWordRepository ;
+import static com.sandy.jnmaker.util.ObjectRepository.setWordnicAdapter ;
 
 import java.awt.SplashScreen ;
+import java.io.File ;
 
 import javax.swing.SwingUtilities ;
 
 import org.apache.log4j.Logger ;
+import org.eclipse.xtext.nodemodel.ICompositeNode ;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils ;
 
 import com.sandy.common.bus.EventBus ;
 import com.sandy.common.objfactory.SpringObjectFactory ;
@@ -20,14 +35,19 @@ import com.sandy.jnmaker.ui.helper.ProjectManager ;
 import com.sandy.jnmaker.util.AppConfig ;
 import com.sandy.jnmaker.util.ConfiguratorBuilder ;
 import com.sandy.jnmaker.util.JNMCommandLine ;
+import com.sandy.jnmaker.util.ObjectRepository ;
 import com.sandy.jnmaker.util.WordRepository ;
 import com.sandy.jnmaker.util.WordnicAdapter ;
+import com.sandy.jnmaker.util.XTextModelParser ;
+import com.sandy.xtext.joveNotes.JoveNotes ;
+import com.sandy.xtext.joveNotes.NotesElement ;
 
 public class JoveNotesMaker {
     
     private static final Logger logger = Logger.getLogger( JoveNotesMaker.class ) ;
     
     public static final String APP_ID = "jnmaker" ;
+    private XTextModelParser modelParser = null ;
     
     public void launch( String[] args ) throws Exception {
         
@@ -45,6 +65,44 @@ public class JoveNotesMaker {
                 }
             }
         } );
+        
+//        testParsing() ;
+    }
+    
+    private void testParsing() throws Exception {
+        
+        modelParser = new XTextModelParser( "com.sandy.xtext.JoveNotesStandaloneSetup" ) ;
+        for( File dir : ObjectRepository.getAppConfig().getJNSrcDirs() ) {
+            recurseAndProcessDir( dir ) ;
+        }
+    }
+    
+    private void recurseAndProcessDir( File dir ) throws Exception {
+        
+        if( dir.isFile() ) {
+            if( dir.getName().endsWith( ".jn" ) ) {
+                parseJNFile( dir ) ;
+            }
+        }
+        else {
+            File[] files = dir.listFiles() ;
+            for( File file : files ) {
+                recurseAndProcessDir( file ) ;
+            }
+        }
+    }
+    
+    private void parseJNFile( File file ) throws Exception {
+        
+        JoveNotes ast = ( JoveNotes )modelParser.parseFile( file ) ;
+        
+        for( NotesElement element : ast.getNotesElements() ) {
+            ICompositeNode cmpNode = NodeModelUtils.getNode( element ) ;
+            if( cmpNode != null ) {
+                String sourceText = cmpNode.getText() ;
+                logger.info( sourceText ) ;
+            }
+        }
     }
     
     private void preInitialize( String[] args ) throws Exception {
---
+++ src/main/java/com/sandy/jnmaker/util/XTextModelParser.java
@@ -0,0 +1,49 @@
+package com.sandy.jnmaker.util;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+public class XTextModelParser {
+
+    private static final String DEF_EMF_REG_METHOD = "createInjectorAndDoEMFRegistration" ;
+
+    public XTextModelParser( String bootstrapClassName )
+        throws Exception {
+        this( bootstrapClassName, DEF_EMF_REG_METHOD ) ;
+    }
+    
+    public XTextModelParser( String bootstrapClassName, 
+                             String bootstrapMethodName )
+        throws Exception { 
+        
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader() ; 
+        Class<?> cls = classLoader.loadClass( bootstrapClassName ) ;
+        Method   mth = cls.getMethod( bootstrapMethodName ) ;
+        Object   obj = cls.newInstance() ;
+        
+        mth.invoke( obj ) ;
+    }
+    
+    public EObject parseFile( File file ) 
+        throws Exception {
+        
+        return parseURL( file.toURI().toURL() ) ;
+    }
+    
+    public EObject parseURL( URL url ) throws Exception {
+        
+        ResourceSet rs       = new ResourceSetImpl();
+        URI         emfURI   = URI.createURI( url.toURI().toString() ) ;
+        Resource    resource = rs.getResource( emfURI, true ) ;
+        EObject     eObj     = resource.getContents().get( 0 ) ;
+
+        return eObj ;
+    }
+}
