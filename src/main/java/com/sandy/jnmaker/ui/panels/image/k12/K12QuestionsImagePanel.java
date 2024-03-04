d342873856f51009f20f5827231acad19abcf774
Sandeep Deb <deb.sandeep@gmail.com>
2024-03-04 12:27:10 +0530

    Usability for part K12 questions

--- src/main/java/com/sandy/jnmaker/ui/panels/image/k12/K12QuestionsImagePanel.java
+++ src/main/java/com/sandy/jnmaker/ui/panels/image/k12/K12QuestionsImagePanel.java
@@ -2,12 +2,15 @@ package com.sandy.jnmaker.ui.panels.image.k12;
 
 import java.awt.event.ActionListener ;
 import java.io.File ;
+import java.io.FileFilter;
+import java.text.DecimalFormat;
 
 import com.sandy.common.ui.CloseableTabbedPane.TabCloseListener ;
 import com.sandy.common.ui.ScalableImagePanel.ScalableImagePanelListener ;
 import com.sandy.jnmaker.ui.panels.image.AbstractImagePanel ;
 import com.sandy.jnmaker.ui.panels.image.SaveFileNameHelperAccessory;
 import com.sandy.jnmaker.ui.panels.image.SaveFnKeyHandler ;
+import org.apache.log4j.Logger;
 
 import javax.swing.*;
 
@@ -15,8 +18,10 @@ import static java.awt.event.KeyEvent.*;
 
 public class K12QuestionsImagePanel extends AbstractImagePanel<K12ExerciseQuestion>
     implements ActionListener, TabCloseListener, ScalableImagePanelListener {
+    
+    static final Logger log = Logger.getLogger( K12QuestionsImagePanel.class ) ;
 
-    public final static String ID = "K12" ;
+    public final static String ID  = "K12" ;
     
     private final SaveFnKeyHandler nextMajorElementSDHandler =
         new SaveFnKeyHandler( "nextMajorElement" ) {
@@ -84,6 +89,25 @@ public class K12QuestionsImagePanel extends AbstractImagePanel<K12ExerciseQuesti
                 if( currentlyDisplayedQuestion != null ) {
                     currentlyDisplayedQuestion.incrementChapterNumber() ;
                     updateSaveDialogFileName( currentlyDisplayedQuestion.getFileName() ) ;
+                    
+                    if( lastSavedDir != null ) {
+                        File subRoot = new File( lastSavedDir, "../../.." ) ;
+                        File[] chapters = subRoot.listFiles( File::isDirectory ) ;
+                        if( chapters != null ) {
+                            for( File chapter : chapters ) {
+                                String nextChapterNameStart = String.format( "%02d",
+                                        currentlyDisplayedQuestion.getChapterNum() ) + " - " ;
+                                if( chapter.getName().startsWith( nextChapterNameStart ) ) {
+                                    File exercisesDir = new File( chapter, "img/exercise" ) ;
+                                    if( !exercisesDir.exists() ) {
+                                        exercisesDir.mkdirs() ;
+                                    }
+                                    lastSavedDir = exercisesDir ;
+                                    saveFileChooser.setCurrentDirectory( lastSavedDir ) ;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         } ;
